65c8ada792b4ab019e021674601b3c21da6af532
Astro <astro@astro-OptiPlex-780.(none)>
2013-12-24 13:40:19 -0500

    Build script change

--- projects/build_boost_android.sh
+++ projects/build_boost_android.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-echo "Please enter your android ndk path:"
-echo "For example:/home/astro/android-ndk-r8e"
-read Input
-echo "You entered:$Input"
+if [[ $1 == *android-ndk-* ]]; then
+	echo "----------------- NDK Path is : $1 ----------------"
+	Input=$1;
+else
+	echo "Please enter your android ndk path:"
+	echo "For example:/home/astro/android-ndk-r8e"
+	read Input
+	echo "You entered:$Input"
 
-echo "----------------- Exporting the android-ndk path ----------------"
+	echo "----------------- Exporting the android-ndk path ----------------"
+fi
 
 #Set path
 export PATH=$PATH:$Input:$Input/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin
 
 #create install directories
+mkdir -p ./android
 mkdir -p ./../../build
 mkdir -p ./../../build/android
-mkdir -p ./android
 
 echo "----------- Building boost 1.53.0 for ANDROID platform -----------------"
 
